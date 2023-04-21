e708f6b63958dccf7ae18a1599fee1ab5f3652f1
Aleksandr Kursov <alexander.kursov@gmail.com>
2023-04-22 01:09:14 +0500

    added optimization of the solution of Task_02 HW_1_Python

--- Task_02.py
+++ Task_02.py
@@ -6,8 +6,7 @@ number = int(input('Enter a three-digit number: '))
 if number > 99 and number < 1000:
     sum = 0
     while number != 0:
-        temp = number % 10
-        sum += temp
+        sum += number % 10
         number = number // 10
     print(f'The sum of the digits of a three-digit number: {sum}')
     print()
